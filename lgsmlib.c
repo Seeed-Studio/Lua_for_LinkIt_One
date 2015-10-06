f60d19fc31ea703651ae69e8c715ec357d45c058
Yihui Xiong <xiongyihui3@gmail.com>
2015-10-06 18:00:16 +0800

    fix number format

--- lgsmlib.c
+++ lgsmlib.c
@@ -5,17 +5,22 @@
 #include "vmtel.h"
 #include "vmlog.h"
 #include "vmchset.h"
+#include "vmsim.h"
+#include "vmsms.h"
 
 #include "lua.h"
 #include "lauxlib.h"
 
 enum VoiceCall_Status { IDLE_CALL, CALLING, RECEIVINGCALL, TALKING };
 
-static void (*g_call_state_changed_callback)(VMINT8) = NULL;
+int g_gsm_incoming_call_cb_ref = LUA_NOREF;
+int g_gsm_new_message_cb_ref = LUA_NOREF;
 
 vm_ucm_id_info_struct g_uid_info;
 int g_call_status = IDLE_CALL;
-char g_number[42];
+char g_incoming_number[42];
+
+extern lua_State *L;
 
 void call_listener_func(vm_call_listener_data *data) {
   if (data->type_op == VM_UCM_INCOMING_CALL_IND) {
@@ -24,15 +29,21 @@ void call_listener_func(vm_call_listener_data *data) {
     g_uid_info.call_id = ind->uid_info.call_id;
     g_uid_info.group_id = ind->uid_info.group_id;
     g_uid_info.call_type = ind->uid_info.call_type;
-    strcpy(g_number, (char *)ind->num_uri);
+    strcpy(g_incoming_number, (char *)ind->num_uri);
     g_call_status = RECEIVINGCALL;
+
+    if (g_gsm_incoming_call_cb_ref != LUA_NOREF) {
+        lua_rawgeti(L, LUA_REGISTRYINDEX, g_gsm_incoming_call_cb_ref);
+        lua_pushstring(L, g_incoming_number);
+        lua_call(L, 1, 0);
+    }
   } else if (data->type_op == VM_UCM_OUTGOING_CALL_IND) {
     vm_ucm_outgoing_call_ind_struct *ind =
         (vm_ucm_outgoing_call_ind_struct *)data->data;
     g_uid_info.call_id = ind->uid_info.call_id;
     g_uid_info.group_id = ind->uid_info.group_id;
     g_uid_info.call_type = ind->uid_info.call_type;
-    strcpy(g_number, (char *)ind->num_uri);
+    strcpy(g_incoming_number, (char *)ind->num_uri);
     g_call_status = TALKING;
   } else if (data->type_op == VM_UCM_CONNECT_IND) {
     vm_ucm_connect_ind_struct *ind = (vm_ucm_connect_ind_struct *)data->data;
@@ -141,6 +152,74 @@ int gsm_hang(lua_State *L) {
   return 1;
 }
 
+int gsm_on_incoming_call(lua_State *L)
+{
+    lua_pushvalue(L, 1);
+    g_gsm_incoming_call_cb_ref = luaL_ref(L, LUA_REGISTRYINDEX);
+
+    return 0;
+}
+
+/* The callback of sending SMS, for checking if an SMS is sent successfully. */
+void _gsm_text_callback(int result){
+  vm_log_debug("send sms callback, result = %d", result);
+}
+
+int gsm_text(lua_State *L)
+{
+    VMWCHAR number[42];
+    VMWCHAR content[100];
+    const char *phone_number = luaL_checkstring(L, 1);
+    const char *message = luaL_checkstring(L, 2);
+
+    vm_ascii_to_ucs2(content, 100*2, message);
+	vm_ascii_to_ucs2(number, 42*2, phone_number);
+
+    lua_pushnumber(L, vm_send_sms(number, content, _gsm_text_callback));
+
+    return 1;
+}
+
+int _gsm_on_new_message(vm_sms_event_t* event_data){
+    vm_sms_event_new_sms_t * event_new_message_ptr;
+    vm_sms_new_msg_t * new_message_ptr = NULL;
+    char content[100];
+    /* Checks if this event is for new SMS message. */
+    if(event_data->event_id == VM_EVT_ID_SMS_NEW_MSG){
+        /* Gets the event info. */
+        event_new_message_ptr = (vm_sms_event_new_sms_t *)event_data->event_info;
+
+        /* Gets the message data. */
+        new_message_ptr  =  event_new_message_ptr->msg_data;
+
+        /* Converts the message content to ASCII. */
+        vm_ucs2_to_ascii((VMSTR)content, 100, (VMWSTR)event_new_message_ptr->content);
+
+        if (g_gsm_new_message_cb_ref != LUA_NOREF) {
+            lua_rawgeti(L, LUA_REGISTRYINDEX, g_gsm_new_message_cb_ref);
+            lua_pushstring(L, new_message_ptr->sc_number);
+            lua_pushstring(L, content);
+            lua_call(L, 2, 0);
+        }
+
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int gsm_on_new_message(lua_State *L)
+{
+    int ref;
+    lua_pushvalue(L, 1);
+    g_gsm_new_message_cb_ref = luaL_ref(L, LUA_REGISTRYINDEX);
+
+    lua_pushnumber(L, vm_sms_set_interrupt_event_handler(VM_EVT_ID_SMS_NEW_MSG, _gsm_on_new_message, L));
+
+    return 1;
+}
+
 #undef MIN_OPT_LEVEL
 #define MIN_OPT_LEVEL 0
 #include "lrodefs.h"
@@ -148,6 +227,9 @@ int gsm_hang(lua_State *L) {
 const LUA_REG_TYPE gsm_map[] = {{LSTRKEY("call"), LFUNCVAL(gsm_call)},
                                 {LSTRKEY("answer"), LFUNCVAL(gsm_anwser)},
                                 {LSTRKEY("hang"), LFUNCVAL(gsm_hang)},
+                                {LSTRKEY("on_incoming_call"), LFUNCVAL(gsm_on_incoming_call)},
+                                {LSTRKEY("text"), LFUNCVAL(gsm_text)},
+                                {LSTRKEY("on_new_message"), LFUNCVAL(gsm_on_new_message)},
                                 {LNILKEY, LNILVAL}};
 
 LUALIB_API int luaopen_gsm(lua_State *L) {

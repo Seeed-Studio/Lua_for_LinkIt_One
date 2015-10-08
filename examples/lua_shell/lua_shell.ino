37b15429f9829d90b4cadc0f96bb77391902f5ec
Yihui Xiong <xiongyihui3@gmail.com>
2015-10-08 16:28:41 +0800

    update

--- examples/lua_shell/lua_shell.ino
+++ examples/lua_shell/lua_shell.ino
@@ -1,13 +1,22 @@
-extern "C" {
-#include "lua.h"
-}
 
+#include "LAudio.h"
+#include "LGSM.h"
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+
+lua_State *L = NULL;
+
+extern int luaopen_gpio(lua_State *L);
+extern int luaopen_audio(lua_State *L);
+extern int luaopen_gsm(lua_State *L);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(13, OUTPUT);
-  
+
   for (int i = 0; i < 3; i++) {
     digitalWrite(13, HIGH);
     printf("hello, printf\n");
@@ -16,13 +25,20 @@ void setup() {
     digitalWrite(13, LOW);
     delay(1000);
   }
+
+  L = lua_open();
+  lua_gc(L, LUA_GCSTOP, 0);  /* stop collector during initialization */
+  luaL_openlibs(L);  /* open libraries */
+
+  luaopen_gpio(L);
+  luaopen_audio(L);
+  luaopen_gsm(L);
+
+  lua_gc(L, LUA_GCRESTART, 0);
+
+  luaL_dofile(L, "init.lua");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  {
-    char *argv[] = {"lua", 0};
-    int argc = 1;
-    lua_main(argc, argv);
-  }
+  dotty(L);
 }
